8515d565d25e5e03a56090cf86c7ef7a41e35cca
Olamilekan Omosanya <olamilekanomosanya@gmail.com>
2025-12-07 22:11:08 +0100

    remove readme file

--- app/Mail/DistributorApplicationMail.php
+++ app/Mail/DistributorApplicationMail.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class DistributorApplicationMail extends Mailable implements ShouldQueue
+class DistributorApplicationMail extends Mailable
 {
     use Queueable, SerializesModels;
 
